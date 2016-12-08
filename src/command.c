dc743ef4e4fb3adc0f994f7fe94fa20c7a8045bb
frobiac <frobiac@gmail.com>
2016-12-08 21:20:06 +0100

    [config] Global config in EEPROM.
    [config] Global config in EEPROM.

--- src/command.c
+++ src/command.c
@@ -280,7 +280,8 @@ void handleSubCmd(char c)
             #endif
 
             switch(c) {
-
+                case 'S': save_config(&g_cfg); break;
+                case 'L': load_config(&g_cfg); break;
               #ifdef PS2MOUSE
                 // change sensitivity for initial and normal operation
                 ///@TODO generic interface, always allow '0' (no %256)
@@ -310,6 +311,7 @@ void handleSubCmd(char c)
                     setCommandMode(false);
                     break;    
             }
+            print_config();
             break; // allow consecutive changes to variables
 
         default:
